e2d6eb3025c5681a84140a366043c0354b63eefb
Will Dixon <dixonwille@hotmail.com>
2017-07-25 20:51:52 -0400

    Add copyright to all the files

--- data.go
+++ data.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2017, Technomancers. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package goNTCore
 
 //Data is the interface implemented by types that can read and write Network Table at a low level.
